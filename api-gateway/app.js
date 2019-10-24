566165b5a8371973d0ae8f103263e59bdd37d20b
Justin Singleton <jsingleton@my365.bellevue.edu>
2019-10-24 14:22:00 -0500

    Add code attribution

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,3 +1,14 @@
+/*
+============================================
+; Title: API Gateway
+; Author: Justin Singleton
+; Date: 2019
+; Description: This program demonstrates the
+; use of JavaScript and the ability to 
+; utilize an API.
+;===========================================
+*/
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
