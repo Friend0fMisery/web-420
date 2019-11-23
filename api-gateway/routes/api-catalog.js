506c0644a683fe1c4badb34c7e5b22ffb7d13b3e
Justin Singleton <jsingleton@my365.bellevue.edu>
2019-11-22 18:27:20 -0600

    Add assignment6.3

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -13,8 +13,10 @@ var express = require('express');
 var router = express.Router();
 var auth_controller = require('../controllers/authController');
 
+router.post('/auth/login', auth_controller.user_login);
 router.post('/auth/register', auth_controller.user_register);
 
 router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/logout', auth_controller.user_logout);
 
 module.exports = router;
